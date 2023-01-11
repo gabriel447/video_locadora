7a1bc407a85aa23e7b47010e0c7d486b4bf05590
unknown <fullstackgabe@gmail.com>
2023-01-10 21:22:40 -0300

    arquivo do banco de dados

---
+++ video_locadora.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 11-Jan-2023 às 01:22
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `video_locadora`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `cep` varchar(255) NOT NULL,
+  `cpf` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nome`, `telefone`, `cep`, `cpf`) VALUES
+(1, 'Gabriel dos anjos senra', '41 98799-8159', '81830-610', '091.345.759-01'),
+(2, 'Camila Soares Silva', '41 98799-4122', '81830-640', '741.208.564-98');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `controle`
+--
+
+CREATE TABLE `controle` (
+  `id` int(11) NOT NULL,
+  `nome_cliente` varchar(255) NOT NULL,
+  `nome_filme` varchar(255) NOT NULL,
+  `cod_filme` int(11) NOT NULL,
+  `cpf_cliente` varchar(255) NOT NULL,
+  `data_locado` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `filmes`
+--
+
+CREATE TABLE `filmes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `genero` varchar(255) NOT NULL,
+  `lancamento` int(11) NOT NULL,
+  `disponivel` int(11) NOT NULL,
+  `cod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `filmes`
+--
+
+INSERT INTO `filmes` (`id`, `nome`, `genero`, `lancamento`, `disponivel`, `cod`) VALUES
+(1, '007: Sem tempo para morrer', 'ação', 2022, 1, 2312948),
+(2, 'Casa Comigo', 'romance', 2022, 0, 2131412),
+(3, 'John Wick 4', 'ação', 2022, 0, 4123124),
+(4, 'Jurassic World: Domínio', 'aventura', 2022, 0, 2412355),
+(5, 'Legalmente Loira 3', 'comédia', 2022, 0, 2512355),
+(6, 'Minions 2: A Ascensão de Gru', 'infantil', 2022, 0, 5123566),
+(7, 'Thor: Amor e Trovão', 'ficção', 2022, 0, 7214748),
+(17, 'A volta dos que não foram', 'aventura', 2012, 0, 5213654),
+(18, 'As longas tranças de um careca', 'aventura', 2012, 0, 1234121);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `historicos`
+--
+
+CREATE TABLE `historicos` (
+  `id` int(255) NOT NULL,
+  `cpf_cliente` varchar(255) NOT NULL,
+  `cod_filme` int(11) NOT NULL,
+  `data_devolucao` date NOT NULL,
+  `valor` int(11) NOT NULL,
+  `multa` int(11) NOT NULL,
+  `valor_total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `controle`
+--
+ALTER TABLE `controle`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `filmes`
+--
+ALTER TABLE `filmes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `historicos`
+--
+ALTER TABLE `historicos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `controle`
+--
+ALTER TABLE `controle`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `filmes`
+--
+ALTER TABLE `filmes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de tabela `historicos`
+--
+ALTER TABLE `historicos`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

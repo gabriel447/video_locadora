fbea4e1f81e21b6f684026f69158d4650e8866c5
Gabriel dos Anjos Senra <fullstackgabe@gmail.com>
2023-01-20 01:32:10 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel-production-5b14.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
